b6cefde247308559af13a933bc77454e9359fc56
Duy <thienduy1295@gmail.com>
2016-04-16 11:02:52 +0700

    Bai tap lan 2

---
+++ app/src/main/java/t3h/android/duy/ui/Ui_signupActivity.java
@@ -0,0 +1,13 @@
+package t3h.android.duy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ui_signupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_signup);
+    }
+}
